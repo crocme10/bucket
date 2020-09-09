5dc2d64c673e9c145fe54af8eb8779bcbc6511ad
riendegris <matt@area403.org>
2020-09-09 22:46:33 +0200

    [FEAT]: Setting up a baseline
    Renders a list of items.
    
    Uses tailwindcss, with custom fonts

---
+++ src/components/Item.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto py-24 px-4">
+    <div class="w-full md:w-5/6 mx-auto">
+      <h1 class="font-text text-gray-900">
+        Item
+      </h1>
+    </div>
+  </div>
+</template>
